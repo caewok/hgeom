e87709a5bf99d83600c7d43279046022430f17ba
Michael Enion <caewok_2000@yahoo.com>
2026-03-29 07:12:13 -0700

    🎸 feat |Registration|Add registration function

---
+++ _module.mjs
@@ -0,0 +1,4 @@
+/** @module HGeom */
+
+export { HPointAbstract } from "./HPointAbstract.js";
+export { HPoint2d } from "./HPoint2d.js";
---
+++ registration.js
@@ -0,0 +1,47 @@
+/* globals
+*/
+"use strict";
+
+import * as lib from "./_module.mjs";
+
+import { MODULE_ID, VERSION } from "./const.js"
+// import { CONFIG } from "./config.js";
+
+// Tests
+import "./tests/HPoint2d.test.js";
+
+export function registerHGEOM() {
+  const module = globalThis[MODULE_ID] ??= {};
+  module.CONST ??= {};
+
+  // Check for newer version.
+  if ( module.CONST.VERSION && isNewerVersion(module.CONST.VERSION, VERSION) ) return;
+
+  // Set up library.
+  // module.CONFIG = CONFIG; // TODO: Implement CONFIG.
+  module.CONST = { VERSION };
+  Object.assign(module, lib);
+}
+
+/**
+ * Determine whether a target version is newer than another version.
+ * Assumed format is "0.0.0", with any number of period dividers.
+ * @param {string} testV        The version to test
+ * @param {string} targetV      The target version
+ * @returns {boolean} True if testV > targetV
+ */
+function isNewerVersion(testV, targetV) {
+  targetV = targetV.split(".");
+  testV = testV.split(".");
+  for ( const [idx, targetStr] of targetV.entries() ) {
+    const testStr = testV[idx];
+    if ( !(targetStr || testStr) ) return false;
+    if ( !targetStr ) return true;
+    if ( !testStr ) return false;
+    const targetNum = Number(targetStr);
+    const testNum = Number(testStr);
+    if ( targetNum === testNum ) continue;
+    return testNum > targetNum;
+  }
+  return false;
+}
