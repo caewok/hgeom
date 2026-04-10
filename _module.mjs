79f293989cf6a061c9c2cd5680e2a894ef56bcb0
Michael Enion <caewok_2000@yahoo.com>
2026-04-10 10:10:50 -0700

    🎸 feat|Imports|Fix Ellipse and PointArray imports

--- _module.mjs
+++ _module.mjs
@@ -1,8 +1,8 @@
 /** @module HGeom */
 
-export { HPointAbstract } from "./HPointAbstract.js";
+export { PointArray } from "./HPointAbstract.js";
 export { HPoint2d } from "./HPoint2d.js";
 export { HPoint3d } from "./HPoint3d.js";
 export { Matrix } from "./Matrix.js";
 export { Point2d, Line2d } from "./Point2d.js";
-export { Polygon2d, Circle2d, Elllipse2d, Segment2d, Triangle2d, Quad2d, Rectangle2d } from "./Polygon2d.js";
+export { Polygon2d, Circle2d, Ellipse2d, Segment2d, Triangle2d, Quad2d, Rectangle2d } from "./Polygon2d.js";
