89890741376bf2788585bf6abb842774ec31bffa
caewok <caewok_2000@yahoo.com>
2026-07-28 08:45:26 -0700

    Update Point3d.js

--- 3d/Point3d.js
+++ 3d/Point3d.js
@@ -36,9 +36,9 @@ export class Point3d extends HPoint3d {
   orient(a, b, c) {
     if ( c ^ !c.isVector ) throw Error(`${this.constructor.name}|orient requires 3 points or 2 vectors`);
     if ( c ) {
-      using v1 = b.subtract(a);
-      using v2 = c.subtract(a);
-      using v3 = this.subtract(a);
+      using v1 = b.clone().subtract(a);
+      using v2 = c.clone().subtract(a);
+      using v3 = this.clone().subtract(a);
       return this.constructor.scalarTriple(v1, v2, v3);
     }
     return this.constructor.scalarTriple(a, b, this);
